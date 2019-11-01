bc3a6c12ce004a22d688c66dfcffd0c03caaba49
Alberto Hoyos <jesalbertohyfx@hotmail.com>
2019-11-01 08:45:21 -0700

    Se le puso al alumno

--- Ordenamiento/MainWindow.xaml.cs
+++ Ordenamiento/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Ordenamiento
     public partial class MainWindow : Window
     {
         ObservableCollection<int> miLista = new ObservableCollection<int>();
+        ObservableCollection<Alumno> alumnos = new ObservableCollection<Alumno>();
         public MainWindow()
         {
             InitializeComponent();
@@ -34,7 +35,17 @@ namespace Ordenamiento
             miLista.Add(13);
             miLista.Add(404);
 
-            lstNumeros.ItemsSource = miLista;
+            alumnos.Add(new Alumno("aaaa", 9.1f, 2));
+            alumnos.Add(new Alumno("Jgse", 9.2f, 23));
+            alumnos.Add(new Alumno("Joge", 9.4f, 1));
+            alumnos.Add(new Alumno("Jgse", 9.6f, 0));
+            alumnos.Add(new Alumno("Jose", 9.3f, 3));
+            alumnos.Add(new Alumno("Joge", 8.1f, 3));
+            alumnos.Add(new Alumno("Jdse", 5.1f, 7));
+            alumnos.Add(new Alumno("Jofe", 4.1f, 8));
+
+            lstNumeros.ItemsSource = alumnos;
+          //lstNumeros.ItemsSource = miLista;
         }
 
         private void BtnOrdenar_Click(object sender, RoutedEventArgs e)
@@ -43,18 +54,18 @@ namespace Ordenamiento
             miLista[0] = miLista[3];
             miLista[3] = temp;*/
 
-            int gap, temp, i, j;
-            gap = miLista.Count / 2;
+            int gap, i, j;
+            gap = alumnos.Count / 2;
             //ahhh
-            while(gap > 0)
+            while (gap > 0)
             {
-                for(i=0; i<miLista.Count; i++)
+                for(i=0; i<alumnos.Count; i++)
                 {
-                    if( gap + i < miLista.Count && miLista[i] > miLista[gap + i])
+                    if( gap + i < alumnos.Count && alumnos[i].promedio > alumnos[gap + i].promedio)
                     {
-                        temp = miLista[i];
-                        miLista[i] = miLista[gap + i];
-                        miLista[gap + i] = temp;
+                        var temp = alumnos[i];
+                        alumnos[i] = alumnos[gap + i];
+                        alumnos[gap + i] = temp;
                     }
                 }
                 gap--;
